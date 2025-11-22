ecab05ee7e64b6ba1d8e5b7421d8f5fd8928f083
Nuha <kuththoosnuha@gmail.com>
2025-11-22 12:44:36 +0530

    Add EventPackage model class with attributes and toString()

--- src/com/eventplanner/app/model/EventPackage.java
+++ src/com/eventplanner/app/model/EventPackage.java
@@ -1,4 +1,38 @@
+
 package com.eventplanner.app.model;
 
 public class EventPackage {
+
+    private int id;
+    private String category;
+    private String name;
+    private double price;
+
+    public EventPackage(int id, String category, String name, double price) {
+        this.id = id;
+        this.category = category;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + name + " (" + category + ") Rs." + price;
+    }
 }

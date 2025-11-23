77cf05dd80d7b47b8d91b53ddb38439d4388cf08
suman <sukku292004@gmail.com>
2025-11-23 20:35:50 +0530

    Add Client class extending User with overridden user type--- suman

--- src/com/eventplanner/app/model/Client.java
+++ src/com/eventplanner/app/model/Client.java
@@ -1,4 +1,13 @@
 package com.eventplanner.app.model;
 
-public class Client {
+public class Client extends User {
+
+    public Client(int id, String name, String phone) {
+        super(id, name, phone);
+    }
+
+    @Override
+    public String getUserType() {
+        return "Client";
+    }
 }
